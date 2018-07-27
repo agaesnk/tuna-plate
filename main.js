b6d0e4188bd8fee0e8e4173e7037f7073f99d6dc
Juliocarlos Velez <juliocarlosvelez@gmail.com>
2018-07-27 15:46:54 +0200

    initial commit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 function main () {
   console.log("JS Running");
+  var mainAudio;
+  mainAudio = new Audio("COCO.m4a");
+  document.body.appendChild(mainAudio);
+  mainAudio.play();
 }
 
 window.addEventListener('load', main);
